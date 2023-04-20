f4098c267599731515530fe78687558dd4368654
rocky <ruslandegtyrev@gmail.com>
2023-04-20 09:07:41 +0300

    final

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const API_URL = `http://localhost:5000/api`
-// export const API_URL = `https://substantial-tricolor-tiglon.glitch.me`
 
 const $host = axios.create({
   baseURL: API_URL
